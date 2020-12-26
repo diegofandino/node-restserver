fc93b9982098aa9cf786e83c8dd81f0f1b3b1841
DiegoFandino <diegofandino.u@gmail.com>
2020-12-26 13:03:11 -0500

    Ajuste a config.js

--- server/config/config.js
+++ server/config/config.js
@@ -10,10 +10,10 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 
 let urlDB;
 
-// if (process.env.NODE_ENV === 'dev') {
-//     urlDB = 'mongodb://localhost:27017/cafeNode';
-// } else {
-urlDB = 'mongodb+srv://strider:TsB0J9HW62WdRIqH@clustercafenode.dqooz.mongodb.net/cafeNode?retryWrites=true&w=majority';
-// }
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafeNode';
+} else {
+    urlDB = 'mongodb+srv://strider:TsB0J9HW62WdRIqH@clustercafenode.dqooz.mongodb.net/cafeNode?retryWrites=true&w=majority';
+}
 
 process.env.URLDB = urlDB;
